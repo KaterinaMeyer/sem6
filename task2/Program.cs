3813b8d82eaba1e2e30340daf1340892ddc92b30
Катерина <katttum@ya.ru>
2023-07-11 22:04:59 +0300

    точка пересечения

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение b1 уравнения первой прямой");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1 уравнения первой прямой");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2 уравнения второй прямой");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2 уравнения второй прямой");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
